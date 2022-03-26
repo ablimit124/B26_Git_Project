d3cc2b5d4d74c56da3ee2bb589797dbce9f050d0
ablimit124 <subinur124@yahoo.com>
2022-03-26 15:10:29 -0500

    updated First file

--- src/day0/First.java
+++ src/day0/First.java
@@ -5,8 +5,8 @@ public class First {
     public static void main(String[] args) {
 
 
-        //TODO: Works on this tomorrow
-        // add some cool code
+        System.out.println("java");
+        System.out.println("Working..");
 
         // extra line 1
         // extra line 2
